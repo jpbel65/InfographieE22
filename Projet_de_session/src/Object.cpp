2fabd2e287054554bc9a6b42c875436510d661a9
Antho15 <tyus1500@hotmail.com>
2019-03-05 15:23:26 -0500

    Ajout méchanisme de gestion objet avec id unique
    On peut désormais trouver un objet par son id.

--- Projet_de_session/src/Object.cpp
+++ Projet_de_session/src/Object.cpp
@@ -1,8 +1,26 @@
 #include "Object.h"
 
-Object::Object() {
+std::map<UUID, Object*> Object::ids;
 
+Object::Object() {
+	UUID* tmp = &id;
+	id = *tmp;
+	Object::ids.insert(std::pair<UUID, Object*>(id, this));
 }
 Object::~Object() {
+	ids.erase(id);
+}
 
+Object* Object::findID(UUID objectID) {
+	auto it = ids.find(objectID);
+	if (it != ids.end()) {
+		return it->second;
+	}
+	else {
+		return nullptr;
+	}
+}
+
+UUID Object::getID() {
+	return id;
 }
--- Projet_de_session/src/Object.h
+++ Projet_de_session/src/Object.h
@@ -1,13 +1,27 @@
 #pragma once
 
 #include <list>
+#include <vector>
+#include <map>
+#include <string>
+
+typedef int* UUID;
 
 class Object
 {
+	// pointeur unique comme id
+	UUID id;
+protected:
+	static std::map<UUID, Object*> ids;
 public:
 	std::list<Object*> childrens;
+	
 
 	Object();
 	~Object();
+
+
+	static Object* findID(UUID objectID);
+	UUID getID();
 };
 
